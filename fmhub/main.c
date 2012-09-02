6fe34dbf40ba65e7fbc1a954a90e1d65321207db
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-03 01:29:08 +0900

    Enhanced fmhub to execute a master process.

--- fmhub/main.c
+++ fmhub/main.c
@@ -1,7 +1,15 @@
+#include <sys/types.h>
+#include <sys/module.h>
+#include <sys/param.h>
+#include <sys/syscall.h>
 #include <assert.h>
+#include <err.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <syslog.h>
+#include <unistd.h>
 
 #include <fsyscall/private.h>
 
@@ -29,8 +37,66 @@ negotiate_version_with_shub(struct mhub *mhub)
 }
 
 static int
+find_syscall()
+{
+	struct module_stat stat;
+	int modid;
+	const char *modname = "sys/fmaster";
+
+	modid = modfind(modname);
+	if (modid == -1)
+		err(-1, "Cannot modfind %s", modname);
+
+	stat.version = sizeof(stat);
+	if (modstat(modid, &stat) != 0)
+		err(-1, "Cannot modstat %s", modname);
+
+	return (stat.data.intval);
+}
+
+static void
+exec_master(int syscall_num, int rfd, int wfd, int argc, char *argv[])
+{
+	char **args, *envp[] = { NULL };
+	int i;
+
+	args = (char **)alloca(sizeof(char *) * (argc + 1));
+	for (i = 0; i < argc; i++) {
+		args[i] = (char *)alloca(sizeof(char) * strlen(argv[i]) + 1);
+		strcpy(args[i], argv[i]);
+	}
+	args[i] = NULL;
+
+	syscall(syscall_num, rfd, wfd, args[0], args, envp);
+	err(1, "fmaster_evecve failed");
+	/* NOTREACHED */
+}
+
+static int
 mhub_main(struct mhub *mhub, int argc, char *argv[])
 {
+	int hub2master[2], master2hub[2], rfd, syscall_num, wfd;
+	pid_t pid;
+
+	syscall_num = find_syscall();
+
+	pipe_or_die(hub2master);
+	pipe_or_die(master2hub);
+
+	pid = fork_or_die();
+	if (pid == 0) {
+		close_or_die(hub2master[W]);
+		close_or_die(master2hub[R]);
+
+		rfd = hub2master[R];
+		wfd = master2hub[W];
+		exec_master(syscall_num, rfd, wfd, argc, argv);
+		/* NOTREACHED */
+	}
+
+	close_or_die(hub2master[R]);
+	close_or_die(master2hub[W]);
+
 	negotiate_version_with_shub(mhub);
 
 	return (0);
