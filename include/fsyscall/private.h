033bdae7ca699c5a0cd1d5915e4debbdd2b50eb1
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-06 02:00:07 +0900

    Added macros for tracing.

--- include/fsyscall/private.h
+++ include/fsyscall/private.h
@@ -1,6 +1,14 @@
 #if !defined(FSYSCALL_PRIVATE_H_INCLUDED)
 #define FSYSCALL_PRIVATE_H_INCLUDED
 
+#include <stdio.h>
+
+#define	TRACE(fmt, ...)	do {						\
+	printf("%s:%u " fmt "\n", __FILE__, __LINE__, __VA_ARGS__);	\
+	fflush(stdout);							\
+} while (0)
+#define	TRACE0(msg)	TRACE("%s", (msg))
+
 #define	array_sizeof(a)	(sizeof(a) / sizeof(a[0]))
 
 #endif
