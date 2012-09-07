c1b60b2ed1d738f8e24d166084b169ec144b1302
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-07 22:20:33 +0900

    include/fsyscall/private.h (TRACE): Enhanced to syslog a message.

--- include/fsyscall/private.h
+++ include/fsyscall/private.h
@@ -2,10 +2,12 @@
 #define FSYSCALL_PRIVATE_H_INCLUDED
 
 #include <stdio.h>
+#include <syslog.h>
 
 #define	TRACE(fmt, ...)	do {						\
 	printf("%s:%u " fmt "\n", __FILE__, __LINE__, __VA_ARGS__);	\
 	fflush(stdout);							\
+	syslog(LOG_DEBUG, fmt, __VA_ARGS__);				\
 } while (0)
 #define	TRACE0(msg)	TRACE("%s", (msg))
 
