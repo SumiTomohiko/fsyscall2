c0bfcefecaa0f0e5c6367f634b36fa4a3e2d1ca1
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-09 03:33:45 +0900

    tests/exit/exit.asm: Added a usage manual in a comment.

--- tests/exit/exit.asm
+++ tests/exit/exit.asm
@@ -1,5 +1,12 @@
 %include "../system.inc"
 
+; Usage: exit x x x ...
+; exit exits with status number that is same as number of command line
+; arguments. If you $(exit), exit status is zero. If you $(exit foo bar), exit
+; status is two. Pay attention to that each argument's value has no mean for
+; this program. For example, if you $(exit 42), exit will exit with status of 1.
+; This behavior is unexpected for you, if you expect the status of 42.
+
 section .text
 global _start
 _start:
