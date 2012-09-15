083954cf0ba45c73e331553f7fda6504e6dbc2c5
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-16 04:13:00 +0900

    Fixed the documentation and the test case for exit.asm.

--- tests/amd64/exit/exit.asm
+++ tests/amd64/exit/exit.asm
@@ -1,10 +1,10 @@
 
 ; Usage: exit x x x ...
 ; exit exits with status number that is same as number of command line
-; arguments. If you $(exit), exit status is zero. If you $(exit foo bar), exit
-; status is two. Pay attention to that each argument's value has no mean for
-; this program. For example, if you $(exit 42), exit will exit with status of 1.
-; This behavior is unexpected for you, if you expect the status of 42.
+; arguments. If you $(exit), exit status is one. If you $(exit foo bar), exit
+; status is three. Pay attention to that each argument's value has no mean for
+; this program. For example, if you $(exit 42), exit will exit with status of
+; two. This behavior is unexpected for you, if you expect the status of 42.
 
 global _start
 _start:
