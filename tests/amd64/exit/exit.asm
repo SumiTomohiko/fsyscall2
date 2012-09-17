68618635b5d181ff2a328e4dac82d50ce3b4c00e
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-09-17 13:04:17 +0900

    tests/amd64/exit/exit.asm: Changed to exit with status of command line arguments number minus one.

--- tests/amd64/exit/exit.asm
+++ tests/amd64/exit/exit.asm
@@ -1,13 +1,14 @@
 
 ; Usage: exit x x x ...
 ; exit exits with status number that is same as number of command line
-; arguments. If you $(exit), exit status is one. If you $(exit foo bar), exit
-; status is three. Pay attention to that each argument's value has no mean for
+; arguments. If you $(exit), exit status is zero. If you $(exit foo bar), exit
+; status is two. Pay attention to that each argument's value has no mean for
 ; this program. For example, if you $(exit 42), exit will exit with status of
-; two. This behavior is unexpected for you, if you expect the status of 42.
+; one. This behavior is unexpected for you, if you expect the status of 42.
 
 global _start
 _start:
 	mov	rax, 1
 	mov	rdi, [rdi]
+	dec	rdi
 	syscall
