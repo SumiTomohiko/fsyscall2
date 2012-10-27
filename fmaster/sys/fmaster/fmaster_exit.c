2202088a53c23e93af485fa47c1a381d36e6b44e
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2012-10-28 01:13:08 +0900

    fmaster/sys/fmaster/fmaster_exit.c (sys_fmaster_exit): Fixed to log retval.

--- fmaster/sys/fmaster/fmaster_exit.c
+++ fmaster/sys/fmaster/fmaster_exit.c
@@ -11,12 +11,12 @@ sys_fmaster_exit(struct thread *td, struct fmaster_exit_args *uap)
 {
 	int error, rval;
 
+	rval = uap->rval;
 	LOG(td, LOG_DEBUG, "exit: rval=%d", rval);
 
 	error = fmaster_write_command(td, CALL_EXIT);
 	if (error != 0)
 		exit1(td, -1);
-	rval = uap->rval;
 	error = fmaster_write_int32(td, rval);
 	if (error != 0)
 		exit1(td, -1);
