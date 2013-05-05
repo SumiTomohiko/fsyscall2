874a17c23922ff692984cbb85720229b1813ee72
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-06 01:18:50 +0900

    Implemented ioctl(2) only for master file.

---
+++ fmaster/sys/fmaster/fmaster_ioctl.c
@@ -0,0 +1,23 @@
+#include <sys/sysproto.h>
+
+#include <fsyscall/private/fmaster.h>
+#include <sys/fmaster/fmaster_proto.h>
+
+int
+sys_fmaster_ioctl(struct thread *td, struct fmaster_ioctl_args *uap)
+{
+	struct ioctl_args a;
+	enum fmaster_fd_type type;
+	int fd;
+
+	fd = uap->fd;
+	type = fmaster_type_of_fd(td, fd);
+	if (type == FD_CLOSED)
+		return (EBADF);
+	if (type == FD_SLAVE)
+		return (0);
+	a.fd = fmaster_fds_of_thread(td)[fd].fd_local;
+	a.com = uap->com;
+	a.data = uap->data;
+	return (sys_ioctl(td, &a));
+}
