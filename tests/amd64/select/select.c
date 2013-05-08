7bf8dcb41c1b59041a71d9d6b12f1695d3c85d46
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-09 03:07:39 +0900

    Changed tests/amd64/select/select to be implemented by C with tiny_runtime.

---
+++ tests/amd64/select/select.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <sys/select.h>
+
+int
+main(int argc, char *argv[])
+{
+	fd_set writefds;
+	struct timeval timeout;
+	int fd = 1;
+
+	FD_ZERO(&writefds);
+	FD_SET(fd, &writefds);
+
+	timeout.tv_sec = 1;
+	timeout.tv_usec = 0;
+
+	return (select(fd + 1, NULL, &writefds, NULL, &timeout) == 1 ? 0 : 1);
+}
