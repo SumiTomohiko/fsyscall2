9c4c37985d62dbe7a077690ba016da45a90171c9
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-17 04:12:51 +0900

    Implemented readInteger() and readCommand().

--- java/src/jp/gr/java_conf/neko_daisuki/fsyscall/io/InputSyscallStream.java
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/io/InputSyscallStream.java
@@ -3,16 +3,36 @@ package jp.gr.java_conf.neko_daisuki.fsyscall.io;
 import java.io.IOException;
 import java.io.InputStream;
 
+import jp.gr.java_conf.neko_daisuki.fsyscall.Command;
+
 public class InputSyscallStream {
 
-    private InputStream mStream;
+    private InputStream mIn;
 
-    public InputSyscallStream(InputStream stream) {
-        mStream = stream;
+    public InputSyscallStream(InputStream in) {
+        mIn = in;
     }
 
     public boolean isReady() throws IOException {
-        return 0 < mStream.available();
+        return 0 < mIn.available();
+    }
+
+    public Command readCommand() throws IOException {
+        return Command.fromInteger(readInteger());
+    }
+
+    /**
+     * Reads signed int (32bits). This method cannot handle unsigned int.
+     */
+    public int readInteger() throws IOException {
+        int n = 0;
+        int shift = 0;
+        int m;
+        while (((m = mIn.read()) & 0x80) != 0) {
+            n += ((m & 0x7f) << shift);
+            shift += 7;
+        }
+        return n;
     }
 }
 
