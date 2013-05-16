f6259746799a94b40c6305fdbb242a43afafb5c4
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-17 04:12:16 +0900

    Added SyscallResult class.

---
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/SyscallResult.java
@@ -0,0 +1,12 @@
+package jp.gr.java_conf.neko_daisuki.fsyscall;
+
+public class SyscallResult {
+
+    public int n;
+    public long l;
+    public int errno;
+}
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
