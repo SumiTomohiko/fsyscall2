e06756f7bdfe015a52352910ada832fa9bdf7982
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-20 20:45:28 +0900

    Added methods to write a datum.

--- java/src/jp/gr/java_conf/neko_daisuki/fsyscall/io/OutputSyscallStream.java
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/io/OutputSyscallStream.java
@@ -3,6 +3,8 @@ package jp.gr.java_conf.neko_daisuki.fsyscall.io;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import jp.gr.java_conf.neko_daisuki.fsyscall.Encoder;
+
 public class OutputSyscallStream {
 
     private OutputStream mStream;
@@ -13,6 +15,14 @@ public class OutputSyscallStream {
 
     public void writeByte(byte n) throws IOException {
         mStream.write(new byte[] { n });
+    }
+
+    public void writeInteger(int n) throws IOException {
+        write(Encoder.encodeInteger(n));
+    }
+
+    public void write(byte buffer[]) throws IOException {
+        mStream.write(buffer);
     }
 }
 
