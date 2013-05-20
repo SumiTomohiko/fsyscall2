77f9b6d9bf7d48cea3869bc33faff5a32e1d9ead
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-05-20 20:45:00 +0900

    Added the class to encode.

---
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/Encoder.java
@@ -0,0 +1,29 @@
+package jp.gr.java_conf.neko_daisuki.fsyscall;
+
+import java.util.Arrays;
+
+public class Encoder {
+
+    public static byte[] encodeInteger(int n) {
+        if (n == 0) {
+            return new byte[] { 0 };
+        }
+
+        byte[] buffer = new byte[32 / 7 + 1];
+        int m = n;
+        int pos = 0;
+        while (0 < m) {
+            int l = m >>> 7;
+            buffer[pos] = (byte)((m & 0x7f) + (0 < l ? 0x80 : 0));
+
+            m = l;
+            pos++;
+        }
+
+        return Arrays.copyOf(buffer, pos);
+    }
+}
+
+/**
+ * vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
+ */
