a3523982d4ffed1279b471cfa546b1db6ce6db77
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-03 01:10:27 +0900

    Fixed get_command_name() to exclude offset.

--- lib/command/command.c
+++ lib/command/command.c
@@ -5,8 +5,12 @@
 const char *
 get_command_name(command_t cmd)
 {
+	/*
+	 * The following expression assumes that CALL_EXIT is the first command.
+	 */
+	int index = cmd - CALL_EXIT;
 	const char *name_of_command[] = CALL_NAMES;
 	const char *name_of_ret[] = RET_NAMES;
 
-	return (((cmd & 1) == 0 ? name_of_command : name_of_ret)[cmd >> 1]);
+	return (((index & 1) == 0 ? name_of_command : name_of_ret)[index >> 1]);
 }
