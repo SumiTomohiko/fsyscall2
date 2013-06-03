04e06cd3c8570a1d3ea8456eaa112d40bfd4ebbd
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-03 17:35:26 +0900

    Implemented open(2) and close(2) in Java.

--- java/src/jp/gr/java_conf/neko_daisuki/fsyscall/UnixConstants.java
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/UnixConstants.java
@@ -2,7 +2,12 @@ package jp.gr.java_conf.neko_daisuki.fsyscall;
 
 public interface UnixConstants {
 
-    public static int O_CREAT = 0x200;
+    public static final int O_RDONLY = 0x0000;
+    public static final int O_WRONLY = 0x0001;
+    public static final int O_RDWR = 0x0002;
+    public static final int O_ACCMODE = 0x0003;
+
+    public static final int O_CREAT = 0x200;
 }
 
 /**
