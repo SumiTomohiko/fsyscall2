5531a3333378f57f9a3e5b8069b4e785f56db0e5
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-04 14:27:04 +0900

    Added the method to encode long in Java.

--- java/src/jp/gr/java_conf/neko_daisuki/fsyscall/Encoder.java
+++ java/src/jp/gr/java_conf/neko_daisuki/fsyscall/Encoder.java
@@ -4,6 +4,25 @@ import java.util.Arrays;
 
 public class Encoder {
 
+    public static byte[] encodeLong(long n) {
+        if (n == 0) {
+            return new byte[] { 0 };
+        }
+
+        byte[] buffer = new byte[64 / 7 + 1];
+        long m = n;
+        int pos = 0;
+        while (0 < m) {
+            long l = m >>> 7;
+            buffer[pos] = (byte)((m & 0x7f) + (0 < l ? 0x80 : 0));
+
+            m = l;
+            pos++;
+        }
+
+        return Arrays.copyOf(buffer, pos);
+    }
+
     public static byte[] encodeInteger(int n) {
         if (n == 0) {
             return new byte[] { 0 };
