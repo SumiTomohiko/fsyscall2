70bfaac8224054d43722479f396de0781971b181
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-06-27 11:56:52 +0900

    Simplified the code.

--- fmaster/sys/fmaster/fmaster_access_pre_execute.c
+++ fmaster/sys/fmaster/fmaster_access_pre_execute.c
@@ -8,19 +8,12 @@
 #include <sys/fmaster/fmaster_pre_post.h>
 #include <sys/fmaster/fmaster_proto.h>
 
-static int
-access_master(struct thread *td, struct fmaster_access_args *uap)
-{
-
-	return (sys_access(td, (struct access_args *)uap));
-}
-
 int
 fmaster_access_pre_execute(struct thread *td, struct fmaster_access_args *uap, int *error)
 {
 
 	if (fmaster_is_master_file(td, uap->path)) {
-		*error = access_master(td, uap);
+		*error = sys_access(td, (struct access_args *)uap);
 		return (0);
 	}
 
