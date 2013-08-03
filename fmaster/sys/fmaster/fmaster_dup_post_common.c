3a693b6c25dd9ae38f1d2ca46166e0d3d9eafd2f
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-08-03 13:14:22 +0900

    Removed the debugging statement.

--- fmaster/sys/fmaster/fmaster_dup_post_common.c
+++ fmaster/sys/fmaster/fmaster_dup_post_common.c
@@ -10,6 +10,5 @@ fmaster_dup_post_common(struct thread *td, struct fmaster_dup_args *uap)
 {
 	enum fmaster_fd_type type = fmaster_type_of_fd(td, uap->fd);
 
-	log(LOG_DEBUG, "fmaster_dup_post_common(td, uap)");
 	return (fmaster_return_fd(td, type, td->td_retval[0]));
 }
