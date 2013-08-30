3df25faf37342f0b6499a615cd12880c8b730fcc
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-08-31 03:40:56 +0900

    Added the test tool for the behavior of dup(2) and close(2).
    dup(2) increases the reference count of the file, and close(2) decreases
    that. If Java version closes the dup(2)'ed file with one close(2)
    calling, the implementation is wrong. Actually, this is true now.

---
+++ tests/amd64/dup_and_close/dup_and_close.c
@@ -0,0 +1,15 @@
+#include <tiny_runtime.h>
+
+int
+main(int argc, const char *argv[])
+{
+	int oldd, newd;
+	const char *msg = argv[2];
+
+	oldd = argv[1][0] - '0';
+	newd = dup(oldd);
+	close(newd);
+	write(oldd, msg, strlen(msg));
+
+	return (0);
+}
