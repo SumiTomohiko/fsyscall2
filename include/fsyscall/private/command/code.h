a1bb032101f5fd8f84374a0cde62348af0ed6058
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-09 18:23:50 +0900

    Changed to update the headers manually for backward compatibility.

---
+++ include/fsyscall/private/command/code.h
@@ -0,0 +1,63 @@
+#if !defined(FSYSCALL_PRIVATE_COMMAND_CODE_H_INCLUDED)
+#define	FSYSCALL_PRIVATE_COMMAND_CODE_H_INCLUDED
+
+#define	CALL_EXIT		42
+#define	RET_EXIT		43
+#define	CALL_READ		44
+#define	RET_READ		45
+#define	CALL_WRITE		46
+#define	RET_WRITE		47
+#define	CALL_OPEN		48
+#define	RET_OPEN		49
+#define	CALL_CLOSE		50
+#define	RET_CLOSE		51
+#define	CALL_LINK		52
+#define	RET_LINK		53
+#define	CALL_GETPID		54
+#define	RET_GETPID		55
+#define	CALL_GETUID		56
+#define	RET_GETUID		57
+#define	CALL_GETEUID		58
+#define	RET_GETEUID		59
+#define	CALL_ACCESS		60
+#define	RET_ACCESS		61
+#define	CALL_DUP		62
+#define	RET_DUP			63
+#define	CALL_GETEGID		64
+#define	RET_GETEGID		65
+#define	CALL_GETGID		66
+#define	RET_GETGID		67
+#define	CALL_IOCTL		68
+#define	RET_IOCTL		69
+#define	CALL_READLINK		70
+#define	RET_READLINK		71
+#define	CALL_FCNTL		72
+#define	RET_FCNTL		73
+#define	CALL_SELECT		74
+#define	RET_SELECT		75
+#define	CALL_WRITEV		76
+#define	RET_WRITEV		77
+#define	CALL_STAT		78
+#define	RET_STAT		79
+#define	CALL_FSTAT		80
+#define	RET_FSTAT		81
+#define	CALL_LSTAT		82
+#define	RET_LSTAT		83
+#define	CALL_GETDIRENTRIES	84
+#define	RET_GETDIRENTRIES	85
+#define	CALL_ISSETUGID		86
+#define	RET_ISSETUGID		87
+#define	CALL_FSTATFS		88
+#define	RET_FSTATFS		89
+#define	CALL_PREAD		90
+#define	RET_PREAD		91
+#define	CALL_MMAP		92
+#define	RET_MMAP		93
+#define	CALL_LSEEK		94
+#define	RET_LSEEK		95
+#define	CALL_SOCKET		96
+#define	RET_SOCKET		97
+#define	CALL_CONNECT		98
+#define	RET_CONNECT		99
+
+#endif
---
+++ include/fsyscall/private/command/name.h
@@ -0,0 +1,67 @@
+#if !defined(FSYSCALL_PRIVATE_COMMAND_NAME_H_INCLUDED)
+#define	FSYSCALL_PRIVATE_COMMAND_NAME_H_INCLUDED
+
+#define	CALL_NAMES	{	\
+	"CALL_EXIT",		\
+	"CALL_READ",		\
+	"CALL_WRITE",		\
+	"CALL_OPEN",		\
+	"CALL_CLOSE",		\
+	"CALL_LINK",		\
+	"CALL_GETPID",		\
+	"CALL_GETUID",		\
+	"CALL_GETEUID",		\
+	"CALL_ACCESS",		\
+	"CALL_DUP",		\
+	"CALL_GETEGID",		\
+	"CALL_GETGID",		\
+	"CALL_IOCTL",		\
+	"CALL_READLINK",	\
+	"CALL_FCNTL",		\
+	"CALL_SELECT",		\
+	"CALL_WRITEV",		\
+	"CALL_STAT",		\
+	"CALL_FSTAT",		\
+	"CALL_LSTAT",		\
+	"CALL_GETDIRENTRIES",	\
+	"CALL_ISSETUGID",	\
+	"CALL_FSTATFS",		\
+	"CALL_PREAD",		\
+	"CALL_MMAP",		\
+	"CALL_LSEEK",		\
+	"CALL_SOCKET",		\
+	"CALL_CONNECT"		\
+}
+#define	RET_NAMES	{	\
+	"RET_EXIT",		\
+	"RET_READ",		\
+	"RET_WRITE",		\
+	"RET_OPEN",		\
+	"RET_CLOSE",		\
+	"RET_LINK",		\
+	"RET_GETPID",		\
+	"RET_GETUID",		\
+	"RET_GETEUID",		\
+	"RET_ACCESS",		\
+	"RET_DUP",		\
+	"RET_GETEGID",		\
+	"RET_GETGID",		\
+	"RET_IOCTL",		\
+	"RET_READLINK",		\
+	"RET_FCNTL",		\
+	"RET_SELECT",		\
+	"RET_WRITEV",		\
+	"RET_STAT",		\
+	"RET_FSTAT",		\
+	"RET_LSTAT",		\
+	"RET_GETDIRENTRIES",	\
+	"RET_ISSETUGID",	\
+	"RET_FSTATFS",		\
+	"RET_PREAD",		\
+	"RET_MMAP",		\
+	"RET_LSEEK",		\
+	"RET_SOCKET",		\
+	"RET_CONNECT"		\
+}
+
+#endif
