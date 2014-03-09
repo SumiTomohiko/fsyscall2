406eebad88a55198ea4aface3d6d3cc2ee542b7d
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-09 14:58:46 +0900

    Fixed to pass size of struct sockaddr.

--- tests/amd64/connect/connect.c
+++ tests/amd64/connect/connect.c
@@ -12,6 +12,7 @@ main(int argc, char *argv[])
 {
 	struct sockaddr_un *paddr;
 	int sock;
+	socklen_t len;
 	char *sockfile;
 
 	sock = socket(PF_LOCAL, SOCK_STREAM, 0);
@@ -19,10 +20,10 @@ main(int argc, char *argv[])
 		return (1);
 	paddr = (struct sockaddr_un *)&sockaddr;
 	sockfile = argv[1];
-	paddr->sun_len = sizeof(paddr->sun_len) + sizeof(paddr->sun_family) + strlen(sockfile) + 1;
+	paddr->sun_len = len = sizeof(paddr->sun_len) + sizeof(paddr->sun_family) + strlen(sockfile) + 1;
 	paddr->sun_family = AF_UNIX;
 	strcpy(paddr->sun_path, sockfile);
-	if (connect(sock, (struct sockaddr *)paddr, sizeof(sockaddr)) != 0)
+	if (connect(sock, (struct sockaddr *)paddr, len) != 0)
 		return (2);
 
 	return (0);
