5c0a6ade07f3f05f7145720fb24e93dd289b3248
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-03-17 18:11:40 +0900

    Fixed socket(2) to set a file descriptor.

---
+++ fmaster/sys/fmaster/fmaster_socket_post_execute.c
@@ -0,0 +1,12 @@
+#include <sys/param.h>
+
+#include <fsyscall/private/fmaster.h>
+#include <sys/fmaster/fmaster_pre_post.h>
+#include <sys/fmaster/fmaster_proto.h>
+
+int
+fmaster_socket_post_execute(struct thread *td, struct fmaster_socket_args *uap)
+{
+
+	return (fmaster_return_fd(td, FD_SLAVE, td->td_retval[0]));
+}
