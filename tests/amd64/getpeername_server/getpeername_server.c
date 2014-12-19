cdf54892d27a6c1c07733b3149332279ca3053ce
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-12-19 13:59:53 +0900

    Added a test for getpeername(2) in server.

---
+++ tests/amd64/getpeername_server/getpeername_server.c
@@ -0,0 +1,30 @@
+#include <tiny_runtime.h>
+
+static struct sockaddr_storage addr;
+
+static int
+callback(int s, struct sockaddr *addr, socklen_t addrlen)
+{
+	struct sockaddr_un *paddr;
+	socklen_t namelen;
+	int error;
+
+	paddr = (struct sockaddr_un *)&addr;
+	namelen = sizeof(addr);
+	error = getpeername(s, (struct sockaddr *)paddr, &namelen);
+	if (error != 0)
+		return (error);
+	tr_print_num(strlen(paddr->sun_path));
+
+	return (0);
+}
+
+int
+main(int argc, const char *argv[])
+{
+	int error;
+
+	error = tr_run_client_server(argv[1], callback, NULL);
+
+	return (error);
+}
