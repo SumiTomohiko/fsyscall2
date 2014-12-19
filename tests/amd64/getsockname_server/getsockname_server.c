85332027eb1cca9cf2eb2f4fe604761aee491682
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-12-19 18:19:24 +0900

    Added a test for getsockname(2).

---
+++ tests/amd64/getsockname_server/getsockname_server.c
@@ -0,0 +1,30 @@
+#include <tiny_runtime.h>
+
+static struct sockaddr_storage storage;
+
+static int
+callback(int s, struct sockaddr *addr, socklen_t addrlen)
+{
+	struct sockaddr_un *name;
+	socklen_t namelen;
+	int error;
+
+	name = (struct sockaddr_un *)&storage;
+	namelen = sizeof(storage);
+	error = getsockname(s, (struct sockaddr *)name, &namelen);
+	if (error != 0)
+		return (error);
+	tr_print_str(name->sun_path);
+
+	return (0);
+}
+
+int
+main(int argc, const char *argv[])
+{
+	int error;
+
+	error = tr_run_client_server(argv[1], callback, NULL);
+
+	return (error);
+}
