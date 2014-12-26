8d909672e54747390bf004d6a16f3f934b505157
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2014-12-26 20:23:46 +0900

    Redesigned the test for poll(2) to work for Java.
    java.io.RandomAccessFile#getFilePosition throws the IOException of
    "Illegal seek" for an fifo.
    
    timeouttestd got unused.

--- tests/amd64/poll_timeout/poll_timeout.c
+++ tests/amd64/poll_timeout/poll_timeout.c
@@ -1,25 +1,27 @@
 #include <tiny_runtime.h>
 
-int
-main(int argc, const char *argv[])
+static int
+connect_callback(int s)
 {
 	struct pollfd fds[1];
-	int fd, n;
-	const char *path;
+	int n;
 
-	if (argc != 2)
-		return (1);
-	path = argv[1];
-
-	fd = open(path, O_RDONLY);
-	if (fd == -1)
-		return (2);
-	fds[0].fd = fd;
+	fds[0].fd = s;
 	fds[0].events = POLLIN;
 	fds[0].revents = 0;
 	n = poll(fds, sizeof(fds) / sizeof(fds[0]), 100 /* msec */);
 	if (n != 0)
-		return (3);
+		return (1);
 
 	return (0);
+}
+
+int
+main(int argc, const char *argv[])
+{
+	int error;
+
+	error = tr_run_client_server(argv[1], NULL, connect_callback);
+
+	return (error);
 }
