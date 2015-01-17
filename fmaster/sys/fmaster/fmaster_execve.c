2e10a6499646ae43c7ae31481e61b26c1d85b2c3
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-01-17 23:39:15 +0900

    Enhanced the log format of execve(2).

--- fmaster/sys/fmaster/fmaster_execve.c
+++ fmaster/sys/fmaster/fmaster_execve.c
@@ -6,6 +6,32 @@
 
 #include <sys/fmaster/fmaster_proto.h>
 
+static int
+log_array(struct thread *td, char **p, const char *name)
+{
+	pid_t pid;
+	int error, i;
+	const char *fmt = "fmaster[%d]: execve: %s[%d]=%s\n";
+	const char *fmt2 = "fmaster[%d]: execve: number of %s is %d\n";
+	char buf[1024];
+
+	pid = td->td_proc->p_pid;
+	if (p == NULL) {
+		log(LOG_DEBUG, "fmaster[%d]: execve: %s=NULL\n", pid, name);
+		return (0);
+	}
+
+	for (i = 0; p[i] != NULL; i++) {
+		error = copyinstr(p[i], buf, sizeof(buf), NULL);
+		if (error != 0)
+			return (error);
+		log(LOG_DEBUG, fmt, pid, name, i, buf);
+	}
+	log(LOG_DEBUG, fmt2, pid, name, i);
+
+	return (0);
+}
+
 int
 sys_fmaster_execve(struct thread *td, struct fmaster_execve_args *uap)
 {
@@ -13,11 +39,17 @@ sys_fmaster_execve(struct thread *td, struct fmaster_execve_args *uap)
 	int error;
 	char path[MAXPATHLEN];
 
-	error = copyin(uap->fname, path, sizeof(path));
+	error = copyinstr(uap->fname, path, sizeof(path), NULL);
 	if (error != 0)
 		return (error);
 	pid = td->td_proc->p_pid;
-	log(LOG_DEBUG, "fmaster[%d]: execve: started: path=%s", pid, path);
+	log(LOG_DEBUG, "fmaster[%d]: execve: started: path=%s\n", pid, path);
+	error = log_array(td, uap->argv, "argv");
+	if (error != 0)
+		return (error);
+	error = log_array(td, uap->envv, "envv");
+	if (error != 0)
+		return (error);
 
 	error = sys_execve(td, (struct execve_args *)uap);
 
