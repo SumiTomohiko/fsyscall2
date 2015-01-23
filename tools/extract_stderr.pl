c937d0e1aa83ea1bd0ab6ccdf5077fff6df0f03a
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-01-23 14:11:02 +0900

    Added a tool to extrace stderr from syslog.

---
+++ tools/extract_stderr.pl
@@ -0,0 +1,9 @@
+#!/usr/local/bin/perl
+
+while (<>) {
+    if (m/ kernel: write\(2\) to fd 2: buf\[\d+\]=(0x[0-9a-f]{2}) \(.\)$/) {
+        print chr(hex($1));
+    }
+}
+
+# vim: shiftwidth=4 expandtab softtabstop=4
