f8c35f9400b1dbdeb36b1bdbf941ea6090fb7a22
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-01-26 18:32:02 +0900

    Enhanced the test to reject a relative path.

--- tests/amd64/chdir_open/chdir_open.c
+++ tests/amd64/chdir_open/chdir_open.c
@@ -2,13 +2,18 @@
 int
 main(int argc, const char *argv[])
 {
+	const char *path;
 
 	if (argc < 2)
 		return (1);
 	if (chdir("/") != 0)
 		return (2);
-	if (open(&argv[1][1], O_RDONLY) == -1)
+
+	path = argv[1];
+	if (*path != '/')
 		return (3);
+	if (open(&path[1], O_RDONLY) == -1)
+		return (4);
 
 	return (0);
 }
