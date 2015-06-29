4f7f5781685d45a3e2e7f570dae7cb21b7363615
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-06-29 22:25:13 +0900

    Changed to fill all bits to detect if a slave ignores unused bits.
    sigfillset(3) sets all bits including unused one.

--- tests/amd64/sigprocmask/sigprocmask.c
+++ tests/amd64/sigprocmask/sigprocmask.c
@@ -4,10 +4,8 @@ main(int argc, const char *argv[])
 {
 	sigset_t set;
 
-	if (sigemptyset(&set) == -1)
+	if (sigfillset(&set) == -1)
 		return (1);
-	if (sigaddset(&set, SIGUSR1) == -1)
-		return (2);
 	if (sigprocmask(SIG_BLOCK, &set, NULL) == -1)
 		return (3);
 
