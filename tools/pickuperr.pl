7965063e8812318fcf3a4f77957b9cb25993efb1
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-07-22 22:29:15 +0900

    Changed to use for Android log.

--- tools/pickuperr.pl
+++ tools/pickuperr.pl
@@ -1,8 +1,11 @@
 #!/usr/local/bin/perl
 
+# For android log, use this with $(nkf -Lu -d).
+
 while (<>) {
-    if (m/ kernel: fmaster\[\d+\]: write\(2\) to fd 2: buf\[\d+\]=(0x[0-9a-f]{2}) \(.\)$/) {
-        print chr(hex($1));
+    if (m/\bwrite\(2\) (for|to) fd 2: buf\[\d+\]=(0x[0-9a-f]{2}) \(.\)$/) {
+        print chr(hex($2));
+        next;
     }
 }
 
