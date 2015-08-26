83dffaa078652f39b0663402f00434651b9fbbd2
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-08-26 22:29:06 +0900

    Enhanced close(2) to log more.

--- fmaster/sys/fmaster/fmaster_close.c
+++ fmaster/sys/fmaster/fmaster_close.c
@@ -4,19 +4,29 @@
 #include <fsyscall/private/fmaster.h>
 #include <sys/fmaster/fmaster_proto.h>
 
+static const char *name = "close";
+
 static int
 fmaster_close_main(struct thread *td, struct fmaster_close_args *uap)
 {
 	int (*closef)(struct thread *, int);
 	enum fmaster_file_place place;
 	int error, fd, lfd, refcount;
+	const char *placestr;
 
 	fd = uap->fd;
 	error = fmaster_unref_fd(td, fd, &place, &lfd, &refcount);
 	if (error != 0)
 		return (error);
 
+	placestr = fmaster_str_of_place(place);
+	fmaster_log(td, LOG_DEBUG,
+		    "%s: fd=%d, place=%s, lfd=%d, refcount=%d",
+		    name, fd, placestr, lfd, refcount);
 	if (refcount == 0) {
+		fmaster_log(td, LOG_DEBUG,
+			    "%s: closing fd %d (%s %d)",
+			    name, fd, placestr, lfd);
 		closef = place == FFP_MASTER ? kern_close
 					     : fmaster_execute_close;
 		error = closef(td, lfd);
@@ -32,7 +42,6 @@ sys_fmaster_close(struct thread *td, struct fmaster_close_args *uap)
 {
 	struct timeval time_start;
 	int error;
-	const char *name = "close";
 
 	fmaster_log(td, LOG_DEBUG, "%s: started: fd=%d", name, uap->fd);
 	microtime(&time_start);
