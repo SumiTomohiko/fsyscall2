d72eae7124ca5533aacd6cde79816181f1f98484
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2015-09-17 15:02:54 +0900

    Added tests to detect ENOTSOCK for sendmsg(2), recvmsg(2).

---
+++ tests/amd64/sendmsg_not_socket_slave/sendmsg_not_socket_slave.c
@@ -0,0 +1,34 @@
+
+int
+main(int argc, const char *argv[])
+{
+	struct msghdr msg;
+	struct iovec iov;
+	int fd;
+	const char *path;
+	char datum;
+
+	if (argc < 2)
+		return (1);
+	path = argv[1];
+
+	fd = open(path, O_WRONLY, 0644);
+	if (fd == -1)
+		return (2);
+	datum = '*';
+	iov.iov_base = &datum;
+	iov.iov_len = sizeof(datum);
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+	msg.msg_control = NULL;
+	msg.msg_controllen =  0;
+	msg.msg_flags = 0;
+	if (sendmsg(fd, &msg, 0) != -1)
+		return (3);
+	if (errno != ENOTSOCK)
+		return (4);
+
+	return (0);
+}
