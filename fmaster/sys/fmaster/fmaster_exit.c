a58923eabb5a8a2bdd7721465b2ceb7a11da217d
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-04-25 20:35:20 +0900

    Enhanced to log a result of exit(2).

--- fmaster/sys/fmaster/fmaster_exit.c
+++ fmaster/sys/fmaster/fmaster_exit.c
@@ -1,6 +1,8 @@
 #include <sys/param.h>
 #include <sys/cdefs.h>
 #include <sys/proc.h>
+#include <sys/syslog.h>
+#include <sys/systm.h>
 
 #include <fsyscall/private/command.h>
 #include <fsyscall/private/fmaster.h>
@@ -41,11 +43,15 @@ void
 sys_fmaster_exit(struct thread *td, struct fmaster_exit_args *uap)
 {
 	int error, rval;
+	const char *sysname = "exit";
 
 	rval = uap->rval;
-	fmaster_log(td, LOG_DEBUG, "exit: started: rval=%d", rval);
+	fmaster_log(td, LOG_DEBUG, "%s: started: rval=%d", sysname, rval);
 
 	error = exit_main(td, rval);
+
+	log(LOG_DEBUG, "fmaster[%d]: %s: ended: error=%d (%s)",
+	    td->td_proc->p_pid, sysname, error, fmaster_geterrorname(error));
 
 	exit1(td, error == 0 ? rval : -1);
 	/* NOTREACHED */
