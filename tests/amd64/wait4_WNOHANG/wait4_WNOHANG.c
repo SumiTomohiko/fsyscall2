c03ed8c4aa935e2490178b52fbaa73675137f626
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-07-22 21:06:41 +0900

    Simplify the test for wait4(2) with WNOHANG.

--- tests/amd64/wait4_WNOHANG/wait4_WNOHANG.c
+++ tests/amd64/wait4_WNOHANG/wait4_WNOHANG.c
@@ -12,34 +12,19 @@ parent_main(pid_t child_pid)
 	return (0);
 }
 
-static void
-signal_handler(int sig)
-{
-}
-
 int
 main(int argc, const char *argv[])
 {
-	struct sigaction act;
-	struct timespec t;
 	pid_t child_pid;
 	int retval;
-
-	act.sa_handler = signal_handler;
-	act.sa_flags = 0;
-	if (sigfillset(&act.sa_mask) == -1)
-		return (16);
-	if (sigaction(SIGTERM, &act, NULL) == -1)
-		return (17);
 
 	child_pid = fork();
 	switch (child_pid) {
 	case -1:
 		return (18);
 	case 0:
-		t.tv_sec = 8;
-		t.tv_nsec = 0;
-		nanosleep(&t, NULL);
+		for (;;)
+			;
 		return (0);
 	default:
 		break;
@@ -47,7 +32,7 @@ main(int argc, const char *argv[])
 
 	retval = parent_main(child_pid);
 
-	kill(child_pid, SIGTERM);
+	kill(child_pid, SIGKILL);
 
 	return (retval);
 }
