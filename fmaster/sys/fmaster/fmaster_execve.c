e140cb146d8a7c470b838b136172a017580ca60a
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-09-16 13:39:14 +0900

    Enhanced the logging of the execve(2) to see errors.

--- fmaster/sys/fmaster/fmaster_execve.c
+++ fmaster/sys/fmaster/fmaster_execve.c
@@ -29,27 +29,56 @@ log_array(struct thread *td, char **p, const char *name)
 	return (0);
 }
 
+static int
+execve_main(struct thread *td, struct fmaster_execve_args *uap)
+{
+	int error;
+
+	error = fmaster_close_on_exec(td);
+	if (error != 0)
+		return (error);
+	error = sys_execve(td, (struct execve_args *)uap);
+	if (error != 0)
+		return (error);
+
+	return (0);
+}
+
 int
 sys_fmaster_execve(struct thread *td, struct fmaster_execve_args *uap)
 {
+	struct timeval time_start;
 	int error;
+	const char *sysname = "execve";
 	char path[MAXPATHLEN];
 
 	error = copyinstr(uap->fname, path, sizeof(path), NULL);
 	if (error != 0)
 		return (error);
-	fmaster_log(td, LOG_DEBUG, "execve: started: path=%s", path);
+	fmaster_log(td, LOG_DEBUG, "%s: started: path=%s", sysname, path);
 	error = log_array(td, uap->argv, "argv");
 	if (error != 0)
 		return (error);
 	error = log_array(td, uap->envv, "envv");
 	if (error != 0)
 		return (error);
+	microtime(&time_start);
 
-	error = fmaster_close_on_exec(td);
-	if (error != 0)
-		return (error);
-	error = sys_execve(td, (struct execve_args *)uap);
+	error = execve_main(td, uap);
+
+	switch (error) {
+	case 0:
+	case EACCES:
+	case ENAMETOOLONG:
+	case ENOENT:
+	case ENOEXEC:
+		break;
+	default:
+		fmaster_log(td, LOG_CRIT, "cannot execve(2)");
+		break;
+	}
+
+	fmaster_log_syscall_end(td, sysname, &time_start, error);
 
 	return (error);
 }
