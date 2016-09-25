201276b2084debd153ccc9b2fd087fc673a68070
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-09-25 17:52:14 +0900

    Enhanced the tool not to process only open(2) but also openat(2) etc.

--- tools/unexpected_ENOENT.pl
+++ tools/unexpected_ENOENT.pl
@@ -5,26 +5,39 @@ use warnings;
 
 my @knowns = [];
 
+sub defined_or_empty {
+    my ($s) = @_;
+    return defined($s) ? $s : "";
+}
+
 sub key {
     my ($pid, $tid) = @_;
-    return $pid . $tid;
+    return $pid . defined_or_empty($tid);
 }
 
 my %pathes = ();
 
 while (<>) {
-    if (m/\bfmaster\[(\d+)\]: (?:tid=(0x[0-9a-f]+): )?open: started: path=\"(.*)\"/) {
-        my ($pid, $tid, $path) = ($1, defined($2) ? $2 : "", $3);
+    if (m/\bfmaster\[(\d+)\]: (?:tid=(0x[0-9a-f]+): )?(?:access|open|stat|lstat): started: path=\"(.*)\"/) {
+        my ($pid, $tid, $path) = ($1, $2, $3);
         $pathes{key($pid, $tid)} = $path;
         next;
     }
-    if (m/\bfmaster\[(\d+)\]: (?:tid=(0x[0-9a-f]+): )?open: ended: .*, error=2 \(ENOENT\)/) {
-        my ($pid, $tid) = ($1, defined($2) ? $2 : "");
+    if (m/\bfmaster\[(\d+)\]: (?:tid=(0x[0-9a-f]+): )?openat: started: fd=-?\d+, path=\"(.*)\"/) {
+        my ($pid, $tid, $path) = ($1, $2, $3,);
+        $pathes{key($pid, $tid)} = $path;
+        next;
+    }
+    if (m/\bfmaster\[(\d+)\]: (?:tid=(0x[0-9a-f]+): )?(access|open|openat|stat|lstat): ended: .*, error=2 \(ENOENT\)/) {
+        my ($pid, $tid, $sysname) = ($1, $2, $3);
         my $key = key($pid, $tid);
         my $path = $pathes{$key};
         delete $pathes{$key};
-        if (!grep { $_ eq $path } @knowns) {
-            printf "pid=%s, tid=%s, path=%s\n", $pid, $tid, $path;
+
+        $path = defined($path) ? $path : "unkown";
+        if (!grep { $path =~ m/$_/ } @knowns) {
+            my $fmt = "pid=%s, tid=%s, sysname=%s, path=%s\n";
+            printf $fmt, $pid, defined_or_empty($tid), $sysname, $path;
         }
         next;
     }
