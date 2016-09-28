469b44d0627c2daa2c565580062c90555a4ea04d
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2016-09-28 21:36:22 +0900

    Updated the master files setting for gedit/inkscape.

--- tools/unexpected_ENOENT.pl
+++ tools/unexpected_ENOENT.pl
@@ -3,7 +3,49 @@
 use strict;
 use warnings;
 
-my @knowns = [];
+my @knowns = qw[
+    /hicolor$
+    /icon-theme.cache$
+    ^/.config/fontconfig/conf.d$
+    ^/.config/fontconfig/fonts$
+    ^/.config/fontconfig/fonts.conf$
+    ^/.local/share/fonts$
+    ^/bin/
+    ^/etc/nvidia/nvidia-application-profiles-rc$
+    ^/etc/nvidia/nvidia-application-profiles-rc.d/
+    ^/etc/xdg/gtk-3.0/settings.ini$
+    ^/home/\w+/
+    ^/proc/\d+/cmdline$
+    ^/tmp/dbus-\w+$
+    ^/usr/bin/
+    ^/usr/local/bin/
+    ^/usr/local/etc/dbus-1/session-local$
+    ^/usr/local/etc/dconf/profile/user$
+    ^/usr/local/etc/fonts/local.conf$
+    ^/usr/local/etc/gtk-2.0/gtkrc$
+    ^/usr/local/etc/gtk-3.0/settings.ini$
+    ^/usr/local/etc/libmap.d$
+    ^/usr/local/etc/xdg/gtk-2.0/gtkrc$
+    ^/usr/local/etc/xdg/gtk-3.0/settings.ini$
+    ^/usr/local/lib/X11/fonts$
+    ^/usr/local/lib/gedit/plugins/\w+(?!.so)$
+    ^/usr/local/share/create/swatches$
+    ^/usr/local/share/dconf/profile/user$
+    ^/usr/local/share/gtk-2.0/gtkrc$
+    ^/usr/local/share/gtk-3.0/settings.ini$
+    ^/usr/local/share/mime/
+    ^/usr/local/share/nls/C/libc.cat$
+    ^/usr/local/share/nls/libc/C$
+    ^/usr/local/share/themes/Adwaita/gtk-3.0/settings.ini$
+    ^/usr/share/dbus-1/services$
+    ^/usr/share/locale/locale.alias$
+    ^/usr/share/nls/C/libc.cat$
+    ^/usr/share/nls/libc/C$
+    ^/usr/share/nvidia/nvidia-application-profiles-346.96-rc$
+    ^/usr/share/nvidia/nvidia-application-profiles-rc$
+    ^/var/run/console/\w+$
+    lib[-\.\w\+]+.so.\d+
+    nss_(compat|dns|files|nis).so.1];
 
 sub defined_or_empty {
     my ($s) = @_;
